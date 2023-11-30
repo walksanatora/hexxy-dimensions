16daf88cab0874cdb049ba4a60351c6de77b60f1
walksanatora <walkerffo22@gmail.com>
2023-11-29 20:59:18 -0800

    Revert "Reapply "oh my is this ... kinda working""
    This reverts commit 9db6e9106ba888de4379aec818c13c146f92b257.

---
+++ src/main/java/net/walksanator/hexdim/casting/mishap/MishapInvalidRoom.kt
@@ -0,0 +1,30 @@
+package net.walksanator.hexdim.casting.mishap
+
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.mishaps.Mishap
+import at.petrak.hexcasting.api.pigment.FrozenPigment
+import at.petrak.hexcasting.common.lib.HexItems
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import net.minecraft.util.DyeColor
+import net.walksanator.hexdim.util.Room
+import java.util.*
+
+class MishapInvalidRoom(val room: Room,val isCarved: Boolean) : Mishap() {
+    override fun accentColor(ctx: CastingEnvironment, errorCtx: Context): FrozenPigment = FrozenPigment(ItemStack(
+        HexItems.DYE_PIGMENTS[DyeColor.RED]
+    ), UUID.randomUUID())
+
+    override fun errorMessage(ctx: CastingEnvironment, errorCtx: Context): Text? {
+        return if (!isCarved) {
+            Text.literal("The room is not carved yet")
+        } else {
+            Text.literal("The room key is invalid")
+        }
+    }
+
+    override fun execute(env: CastingEnvironment, errorCtx: Context, stack: MutableList<Iota>) {}
+
+
+}
--- src/main/java/net/walksanator/hexdim/util/Room.kt
+++ src/main/java/net/walksanator/hexdim/util/Room.kt
@@ -1,34 +1,59 @@
 package net.walksanator.hexdim.util
 
 import net.walksanator.hexdim.HexxyDimStorage
+import net.walksanator.hexdim.casting.mishap.MishapInvalidRoom
 import kotlin.random.Random
 
-class Room(val rect: Rectangle,val height: Int, var key: Int?) {
+class Room(val rect: Rectangle,val height: Int, var key: Int?, var isDone: Boolean) {
+    companion object {
+        val argc: Int = 7 //this is the number of args
+    }
+
+    constructor(part: IntArray) : this(
+        Rectangle(
+            part[0],
+            part[1],
+            part[2],
+            part[3]
+        ),
+        part[4],
+        part[5],
+        part[6] > 0
+    )
+
     init {
         if (key == null) {
             key = Random.nextInt()
         }
     }
 
-    fun keyCheck(other: Int): Boolean = other == key
+    fun keyCheck(other: Int) {
+        if (!isDone) {throw MishapInvalidRoom(this,false)}
+        if (other != key) {throw MishapInvalidRoom(this,true)}
+    }
+    fun keyCheckNoCarveCheck(other: Int) {
+        if (other != key) {throw MishapInvalidRoom(this,true)}
+    }
+
     fun toIntArray(): IntArray {
-        return intArrayOf(rect.x,rect.y,rect.w,rect.h,height,key!!)
+        val doneInt = if (isDone) {1} else {0}
+        return intArrayOf(rect.x,rect.y,rect.w,rect.h,height,key!!, doneInt)
     }
 
     fun getX(): Int {
-        return rect.x + (HexxyDimStorage.xPad/2)
+        return rect.x + (HexxyDimStorage.X_PADDING/2)
     }
 
     fun getY(): Int {
-        return rect.y + (HexxyDimStorage.yPad/2)
+        return rect.y + (HexxyDimStorage.Y_PADDING/2)
     }
 
     fun getW(): Int {
-        return rect.w - HexxyDimStorage.xPad
+        return rect.w - HexxyDimStorage.X_PADDING
     }
 
     fun getH(): Int {
-        return rect.h - HexxyDimStorage.yPad
+        return rect.h - HexxyDimStorage.Y_PADDING
     }
 
     fun internalToRect(): Rectangle = Rectangle(
